b9e43f9d95bf61a4ec2199cd5185e05a13cb82d0
mihaimorosan <morosanmihai@live.com>
2020-11-06 22:12:12 -0500

    modified AboutTemplate.js, added Content.js

--- src/templates/AboutTemplate.js
+++ src/templates/AboutTemplate.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import Content from '../components/Content';
+import Hero from '../components/Hero'
 
 function AboutTemplate(props) {
 
     return(
-        <p>Yooooo you're at About!</p>
+        <div>
+            <Hero title={props.title}/>
+
+            <Content>
+                <p>Hello! My name is Mihai Morosan and I am fourth-year student at Ryerson University with a major in Computer Science and an interest in Software Engineering, Web Development, and Robotics.</p>
+                
+                <p> Filler to be added later </p>
+            </Content>
+        </div>
     )
 
 }
 
-export default AboutTemplate
+export default AboutTemplate;
